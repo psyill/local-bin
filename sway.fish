f4eafd6c98189be5085c673298b6c0db77fcdb17
Hans Ellegård <psyill.net@gmail.com>
2025-09-06 10:59:34 +0200

    Move graphic related variables to Sway script

--- sway.fish
+++ sway.fish
@@ -1,4 +1,16 @@
 #!/usr/bin/fish
 
-set --export XDG_CURRENT_DESKTOP sway
-exec dbus-run-session sway --unsupported-gpu $argv
+#set --export WLR_NO_HARDWARE_CURSORS 1
+set --export XDG_SESSION_DESKTOP "sway;wlroots"
+
+# https://github.com/swaywm/sway/issues/595
+set --export _JAVA_AWT_WM_NONREPARENTING 1
+
+set --export QT_QPA_PLATFORM wayland-egl
+set --export QT_QPA_PLATFORMTHEME qt5ct
+set --export QT_WAYLAND_DISABLE_WINDOWDECORATION 1
+set --erase QT_STYLE_OVERRIDE
+
+set --export SDL_VIDEODRIVER wayland
+
+exec dbus-run-session -- sway --unsupported-gpu $argv
